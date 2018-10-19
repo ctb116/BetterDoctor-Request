e7de11fefa1431b7d59dda87678bdca8374bb761
Catherine Bradley <catherinetybradley@gmail.com>
2018-10-19 13:29:22 -0700

    add required query returns - address, number, website, new patients

--- src/main.js
+++ src/main.js
@@ -21,17 +21,32 @@ $(document).ready(function() {
           let doctorFistName = (response.data[i].profile).first_name;
           let doctorLastName = (response.data[i].profile).last_name;
 
-          $('#showResults').append(`<div class="panel panel-default">${doctorFistName} ${doctorLastName}</br>`)
+          // $('#showResults').append(`<div class="panel panel-default">${doctorFistName} ${doctorLastName}</br>`)
 
           for(let a=0; a < ((response.data[i]).practices).length; a++) {
             let doctorLocationName = ((response.data[i]).practices[a]).name;
+            let doctorWebsite = ((response.data[i]).practices[a]).website;
             let doctorAddress = ((response.data[i]).practices[a]).visit_address;
             let doctorCity = doctorAddress.city;
             let doctorState = doctorAddress.state_long;
             let doctorStreet = doctorAddress.street;
             let doctorZip = doctorAddress.zip;
+            let acceptsNew = ((response.data[i]).practices[a]).accepts_new_patients;
 
-            $('#showResults').append(`${doctorLocationName}<br> Address: ${doctorStreet}<br>${doctorCity}, ${doctorState} ${doctorZip}</div></br></br>`);
+            for(let e=0; e < (((response.data[i]).practices[a]).phones).length; e++ ) {
+              let doctorNumber = (((response.data[i]).practices[a]).phones[e]).number;
+              let doctorNumberType = (((response.data[i]).practices[a]).phones[e]).type;
+
+            $('#showResults').append(`<div class="panel panel-default">
+                                      ${doctorFistName} ${doctorLastName}</br>
+                                      ${doctorLocationName}<br>
+                                      Address: ${doctorStreet}<br>
+                                      ${doctorCity}, ${doctorState} ${doctorZip}</br>
+                                      Number: ${doctorNumber} Type: ${doctorNumberType}</br>
+                                      Website: ${doctorWebsite}</br>
+                                      Accepts new patients? ${acceptsNew}
+                                      </div></br></br>`);
+            }
           }
         }
       },
