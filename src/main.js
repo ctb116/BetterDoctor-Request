6105bd3852507f0586cb958aba67c1264801cc76
Catherine Bradley <catherinetybradley@gmail.com>
2018-10-19 10:20:42 -0700

    query to return doctor name based on health issue

--- src/main.js
+++ src/main.js
@@ -5,4 +5,29 @@ import './styles.css';
 
 $(document).ready(function() {
 
+  $('#doctor').click(function() {
+    let healthIssue = $("#healthIssue").val();
+    console.log(healthIssue);
+
+    $.ajax({
+      url: `https://api.betterdoctor.com/2016-03-01/doctors?query=${healthIssue}&location=wa-seattle&user_location=37.773%2C-122.413&skip=0&user_key=4773972df8c668e0d05ee58b36ec5d5b`,
+      type: 'GET',
+      data: {
+        format: 'json'
+      },
+      success: function(response) {
+        console.log(response);
+        for(let i=0; i < (response.data).length; i++) {
+          let doctorFistName = (response.data[i].profile).first_name;
+          let doctorLastName = (response.data[i].profile).last_name;
+          console.log(doctorFistName);
+          $('#showResults').append(`${doctorFistName} ${doctorLastName}`);
+          console.log(response.data[i].profile);
+        }
+      },
+      error: function() {
+        $('#errors').text("There was an error processing your request. Please try again.");
+      }
+    });
+  });
 });
